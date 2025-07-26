ebae3758757c27834071381b55cfea74aeec3909
Diana Serbu <serbu.diana.maria@gmail.com>
2025-07-26 22:56:42 +0300

    chore: add function signature docs

--- trap-raining-water/index.js
+++ trap-raining-water/index.js
@@ -1,5 +1,10 @@
 import { Stack } from '../helpers/stack.js'
 
+/**
+ * @description Given n non-negative integers representing an elevation map where the width of each bar is 1, computes how much water it can trap after raining
+ * @param {Array[number]} height
+ * @returns {Number}
+ */
 export function trap (height) {
   const stack = new Stack()
   let trappedWaterBlocks = 0
